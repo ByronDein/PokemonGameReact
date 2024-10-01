92ca16ab288fc0b762e4511b5c4f42c8b5e39ef7
Byron Gonzalez <byrondein@gmail.com>
2024-10-01 17:22:52 -0300

    add quantity of questions, score, and remaining questions

--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -5,41 +5,56 @@ import getPokemonOptions from "../helpers/getPokemonOptions";
 const PokemonPage = () => {
     const [pokemonImg, setPokemonImg] = useState('');
     const [pokemonInfo, setPokemonInfo] = useState([]);
-    const [pokemonId, setPokemonId] = useState(0);
+    const [pokemonSelected, setPokemonSelected] = useState(0);
     const [loading, setLoading] = useState(true);
+    const [score, setscore] = useState(0)
+    const [message, setmessage] = useState('')
+    const [quantityOfQuestions, setquantityOfQuestions] = useState(0)
+    const [remainigQuestions, setremainigQuestions] = useState(15)
 
     useEffect(() => {
+        setLoading(true);
+        setmessage('');
         const fetchPokemonData = async () => {
             try {
                 const options = await getPokemonOptions();
                 setPokemonInfo(options);
                 const randomPokemon = Math.floor(Math.random() * 4);
-                setPokemonId(options[randomPokemon].data.id);
+                setPokemonSelected(options[randomPokemon].data);
             } catch (error) {
                 console.error("Error fetching Pokemon data:", error);
             } finally {
                 setTimeout(() => {
-                    setLoading(false);
-                }, 300);
+                    
+                }, );
             }
         };
-        fetchPokemonData();
-    }, []);
+        if(quantityOfQuestions < remainigQuestions){
+
+            fetchPokemonData();
+        }
+        else {
+            setmessage(`Tu puntuación final es: ${score}`);
+        }
+    }, [score, quantityOfQuestions, remainigQuestions]);
 
     useEffect(() => {
-        if (pokemonId) {
-            setPokemonImg(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`);
+        if (pokemonSelected.id) {
+            setPokemonImg(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonSelected.id}.svg`);
+            setLoading(false);
         }
-    }, [pokemonId]);
+    }, [pokemonSelected]);
 
     const checkAnswer = (id) => {
         console.log(id);
-        console.log(pokemonId);
-        
-        if (parseInt(id) === pokemonId) {
-            console.log('Respuesta correcta');
+        console.log(pokemonSelected.id);
+        setquantityOfQuestions(quantityOfQuestions + 1)
+        if (parseInt(id) === pokemonSelected.id) {
+            setscore(score + 1);
+            setmessage(`Respuesta correcta es: ${pokemonSelected.name}`);
         } else {
-            console.log('Respuesta incorrecta');
+            setmessage(`Respuesta Incorrecta era: ${pokemonSelected.name}`);
+
         }
     }
 
@@ -65,11 +80,13 @@ const PokemonPage = () => {
                     {
                         pokemonInfo.map((pokemon) => (
                             <div className="mt-3" key={pokemon.data.id}>
-                                <button className="p-4 bg-red-500 w-52 text-white" onClick={(e)=> checkAnswer(e.target.value)} value={pokemon.data.id} >{pokemon.data.name}</button>
+                                <button className="p-4 bg-red-500 w-52  text-white" onClick={(e)=> checkAnswer(e.target.value)} value={pokemon.data.id} >{pokemon.data.name}</button>
                             </div>
                         ))
                     }
-                    {pokemonId}
+                    your score is: {score}
+                    <h1>Questions: {quantityOfQuestions}</h1>
+                    <h1>{message}</h1>
                 </div>
             )}
         </>
