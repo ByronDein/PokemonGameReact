3dc262fc599a04119950ac39f5ebe3a35a407f9b
Byron Gonzalez <byrondein@gmail.com>
2024-10-06 00:03:38 -0300

    change finalScore to use usecontext for the score

--- src/pages/FinalScore.jsx
+++ src/pages/FinalScore.jsx
@@ -1,21 +1,22 @@
-import { useNavigate, useLocation } from "react-router-dom"
-const FinalScore = () => {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const score = searchParams.get('score');
-  const quantityOfQuestions = searchParams.get('quantityOfQuestions');
-  const navigate = useNavigate();
-  const newGame = () => {
-    navigate('/');
-  }
-  return (
-    <div className="flex flex-col justify-center items-center mt-20">
-      <h2>¡Felicidades! Has completado el juego</h2>
-      <h3>Tu puntuación final es: {score} de {quantityOfQuestions}</h3>
-      <button className="p-2 bg-red-300 rounded-lg" onClick={newGame}>Jugar de nuevo</button>
+import { useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import { ScoreContext } from '../contexts/ScoreContext';
 
-    </div>
-  )
+const FinalScore = () => {
+    const { score, quantityOfQuestions } = useContext(ScoreContext);
+    const navigate = useNavigate();
+
+    const newGame = () => {
+        navigate('/');
+    }
+
+    return (
+        <div className="flex flex-col justify-center items-center mt-20">
+            <h2>¡Felicidades! Has completado el juego</h2>
+            <h3>Tu puntuación final es: {score} de {quantityOfQuestions}</h3>
+            <button className="p-2 bg-red-300 rounded-lg" onClick={newGame}>Jugar de nuevo</button>
+        </div>
+    )
 }
 
-export default FinalScore
+export default FinalScore;
